afcb2b996d515a9f7dd25236829ab00189b30323
Suman Gurung <suman_201900250@smit.smu.edu.in>
2023-01-21 22:44:19 +0530

    added all levels , completed game

--- test.py
+++
@@ -1,7 +0,0 @@
-import random
-
-movement_info = [[[50,550],[-100,-50]] , [[650,700],[50,550]] , [[650,700],[50 , 550]] ,[[-50,-100],[50,550]]]
-
-index = random.choice([0,1,2,3])
-
-print(random.randrange(-10,10,-1))
